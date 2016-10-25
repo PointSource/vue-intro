ebfa62779f9cdc63e83ba23f6a5d5db5b171ee90
Greg Considine <greg@greg-considine.com>
2016-10-25 09:57:33 -0400

    Add post-cli structure

--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App';
 
-Vue.use(VueRouter);
-
-import Detail from 'components/Detail.vue';
-
-var router = new VueRouter({
-  mode: 'history',
-  routes: [
-    { name: 'home', path: '/', component: App },
-    { name: 'detail', path: '/detail/:articleId', component: Detail },
-    { path: '*', redirect: '/' }
-  ]
-});
-
 /* eslint-disable no-new */
-new Vue({ router }).$mount('#app');
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+});
